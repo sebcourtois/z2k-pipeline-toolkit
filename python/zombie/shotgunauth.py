8e9d4fbe4fbc1311549020acd3c7c39c525766f2
sebcourtois <neitsabes974@gmail.com>
2015-07-07 20:59:20 +0200

    shotgun authentication through shotgunauth.ShotgunAuth class

---
+++ python/zombie/shotgunauth.py
@@ -0,0 +1,21 @@
+
+from pytk.core.authenticator import Authenticator
+
+from zombie.shotgunengine import ShotgunEngine
+
+class ShotgunAuth(Authenticator):
+
+    def __init__(self):
+        super(ShotgunAuth, self).__init__()
+        self._shotgun = ShotgunEngine()
+
+    def loggedUser(self, *args, **kwargs):
+        userData = self._shotgun.getLoggedUser(*args, **kwargs)
+        return userData
+
+    def login(self, *args, **kwargs):
+        userData = self._shotgun.loginUser(*args, **kwargs)
+        return userData
+
+    def logout(self, *args, **kwargs):
+        self._shotgun.logoutUser(*args, **kwargs)
