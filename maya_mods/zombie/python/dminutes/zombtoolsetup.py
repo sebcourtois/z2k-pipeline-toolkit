bf1d92c40549e1b41550ac91708f51edc6b2ffe8
sebcourtois <neitsabes974@gmail.com>
2015-07-22 20:13:09 +0200

    added python module 'zombtoolsetup.py' in maya_mods. added 'requests' lib in main python directory: a 'damas.py' dependency

---
+++ maya_mods/zombie/python/dminutes/zombtoolsetup.py
@@ -0,0 +1,18 @@
+
+
+from pytaya.mayatoolsetup import MayaToolSetup
+
+''
+#-------------------------------------------------------------------------------
+#    Core
+#-------------------------------------------------------------------------------
+
+class ZombToolSetup(MayaToolSetup):
+
+    classMenuName = "zombToolMenu"
+    classMenuLabel = "Zomb Tools"
+
+
+    def populateMenu(self):
+        pass
+
