7cc2e34cde1fb5186111eea33f45633e04b7b359
Cyril Gibaud <cyril.gibaud@toonkit-studio.com>
2015-08-11 18:47:40 +0200

    changed the project id mecanism in shotgunengine.py (now works with a string member "projectName" and/or as first argument in constructor minor update to sceneManager/sceneManagerUI

--- maya_mods/zombie/python/dminutes/sceneManagerUI.py
+++ maya_mods/zombie/python/dminutes/sceneManagerUI.py
@@ -9,45 +9,48 @@ PROJECTNAME = "zombillenium"
 PROJECT = None
 
 def sceneManagerUI():
-	global PROJECT
+    global PROJECT
 
-	if (pc.window('sceneManagerUI', q=True, exists=True)):
-		pc.deleteUI('sceneManagerUI')
+    if (pc.window('sceneManagerUI', q=True, exists=True)):
+        pc.deleteUI('sceneManagerUI')
 
-	dirname, filename = os.path.split(os.path.abspath(__file__))
-	ui = pc.loadUI(uiFile=dirname + "/UI/sceneManagerUI.ui")
-	connectCallbacks()
+    dirname, filename = os.path.split(os.path.abspath(__file__))
+    ui = pc.loadUI(uiFile=dirname + "/UI/sceneManagerUI.ui")
+    connectCallbacks()
 
-	#Initialize DAVOS
-	DamProject = damproject.DamProject
+    #Initialize DAVOS
+    DamProject = damproject.DamProject
 
-	PROJECT = DamProject(PROJECTNAME)
+    PROJECT = DamProject(PROJECTNAME)
 
-	if PROJECT == None:
-		pc.error("Cannot initialize project '{0}'".format(PROJECTNAME))
+    if PROJECT == None:
+        pc.error("Cannot initialize project '{0}'".format(PROJECTNAME))
 
-	initialize()
-	pc.showWindow(ui)
+    initialize()
+    pc.showWindow(ui)
 
 
 def initialize():
-	#User info
-	print PROJECT._shotgundb.currentUser
-	pc.textField('sm_user_bt', edit=True, text=PROJECT._shotgundb.currentUser['name'])
+    #User info
+    print PROJECT._shotgundb.currentUser
+    pc.textField('sm_user_bt', edit=True, text=PROJECT._shotgundb.currentUser['name'])
 
-	pc.textScrollList("sm_step_dd", edit=True, removeAll=True)
+    #pc.textScrollList("sm_step_dd", edit=True, removeAll=True)
 
-	allowedsteps = [s['name'] for s in PROJECT._shotgundb.currentUser['sg_allowedsteps']]
+    items = pc.optionMenu('sm_step_dd', query=True, itemListShort=True)
+    for item in items:
+        pc.deleteUI(item)
 
-	pc.optionMenu("sm_step_dd", edit=True, append=allowedsteps)
+    for allowedstep in PROJECT._shotgundb.currentUser['sg_allowedsteps']:
+        pc.menuItem("sm_step_" + allowedstep['name'], label= allowedstep['name'], parent='sm_step_dd')
 
 #Callbacks management
 
 def connectCallbacks():
-	print 'connectCallbacks'
-	pc.button('sm_disconnect_bt', edit=True, c=doDisconnect)
+    print 'connectCallbacks'
+    pc.button('sm_disconnect_bt', edit=True, c=doDisconnect)
 
 def doDisconnect(*args):
-	print 'doDisconnect'
-	PROJECT._shotgundb.logoutUser()
-	sceneManagerUI()
+    print 'doDisconnect'
+    PROJECT._shotgundb.logoutUser()
+    sceneManagerUI()
