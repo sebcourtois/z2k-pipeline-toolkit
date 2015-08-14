dce27107bd8179e91f8db77379f9e1d5f005318c
sebcourtois <neitsabes974@gmail.com>
2015-08-14 22:02:40 +0200

    new launchers: davos_gui.bat and eclipse.bat moved "davosengine.py" to "scripts/davos_cmd.py" new "davos_gui.py" script

--- launchers/paris/davos_gui.bat
+++ launchers/paris/davos_gui.bat
@@ -1,4 +1,4 @@
-SET "ENGINE=%~dp0..\..\scripts\davos_cmd.py"
+SET "ENGINE=%~dp0..\..\scripts\davos_gui.py"
 call :resolve "%ENGINE%"
 
 "C:\Python27\python.exe" "%~dp0setup_env_tools.py" True True "C:\Python27\python.exe" -i %_RETURN%
