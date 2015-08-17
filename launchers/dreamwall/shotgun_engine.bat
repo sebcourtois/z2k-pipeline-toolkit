20e365cb688abd50e77342a7947fc798be16b022
sebcourtois <neitsabes974@gmail.com>
2015-08-17 11:42:51 +0200

    relinked "zombie" calls to "zomblib"

--- launchers/dreamwall/shotgun_engine.bat
+++ launchers/dreamwall/shotgun_engine.bat
@@ -1,4 +1,4 @@
-SET "ENGINE=%~dp0\..\..\python\zombie\shotgunengine.py"
+SET "ENGINE=%~dp0\..\..\python\zomblib\shotgunengine.py"
 call :resolve "%ENGINE%"
 
 "C:\Python27\python.exe" "%~dp0\setup_env_tools.py" call "C:\Python27\python.exe" %_RETURN% %*
