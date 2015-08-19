41f51b484a49479def44fef96b3faf47f324d789
sebcourtois <neitsabes974@gmail.com>
2015-08-19 12:28:02 +0200

    fixed "davos_browser" script crash: properly hooked the window to maya

--- maya_mods/zombie/scripts/zombie/davos_browser.py
+++ maya_mods/zombie/scripts/zombie/davos_browser.py
@@ -3,17 +3,26 @@ import sys
 
 from davos_maya.core.mrclibrary import MrcLibrary
 from davos.gui.assetbrowserwindow import AssetBrowserWindow
+from pytd.util.sysutils import inDevMode
+
+from maya.app.general.mayaMixin import MayaQWidgetBaseMixin
 
 WINDOW_NAME = "assetBrowserWin"
 mainWin = None
+
+class MayaHookWindow(MayaQWidgetBaseMixin, AssetBrowserWindow):
+
+    def __init__(self, parent=None):
+        super(MayaHookWindow, self).__init__(parent=parent)
 
 def launch(argv):
 
     global mainWin
 
-    mainWin = AssetBrowserWindow(WINDOW_NAME)
+    mainWin = MayaHookWindow()
     mainWin.show()
-    mainWin.setProject("zombtest", libraryType=MrcLibrary)
+    sProject = "zombtest" if inDevMode() else "zombillenium"
+    mainWin.setProject(sProject, libraryType=MrcLibrary)
 
 if __name__ == "__main__":
     launch(sys.argv)
