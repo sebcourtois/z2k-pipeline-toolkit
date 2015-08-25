1cd1bf4a96ab569c37e9880ad54aa327df98990c
sebcourtois <neitsabes974@gmail.com>
2015-08-25 15:50:06 +0200

    deleted "davos_gui_DEV.bat": use zombtest/davos_gui.bat

--- launchers/paris/davos_gui_DEV.bat
+++
@@ -1,8 +0,0 @@
-SET "ENGINE=%~dp0\..\..\python\davos-dev\scripts\davos_gui.py"
-call :resolve "%ENGINE%"
-
-"C:\Python27\python.exe" "%~dp0\setup_env_tools.py" launch "C:\Python27\python.exe" %_RETURN% -p zombtest
-
-:resolve
-    set "_RETURN=%~f1"
-    goto :EOF
