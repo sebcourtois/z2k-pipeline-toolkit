7864259060e395298384732c50e41310690e9409
jipee <jipe.oim@gmail.com>
2015-09-10 17:52:38 +0200

    first comitage

--- maya_mods/zombie/python/dminutes/jipeLib.py
+++ maya_mods/zombie/python/dminutes/jipeLib.py
@@ -1,1 +1,19 @@
-print "pouet poil au nez"
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+################################################################
+# Name    : Temp Zomby jipeLib
+# Version : 001
+# Description : all Z2K'scripts related functions
+# Author : Jean-Philippe Descoins
+# Date : 2015_09_10
+# Comment : WIP
+################################################################
+#    ! Toute utilisation de ce se script sans autorisation     #
+#                         est interdite !                      #
+#    ! All use of this script without authorization is         #
+#                           forbidden !                        #
+#                                                              #
+#                                                              #
+#                 © Jean-Philippe Descoins                     #
+################################################################
+
