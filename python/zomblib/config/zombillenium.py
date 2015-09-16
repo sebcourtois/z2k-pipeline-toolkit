062dfb6ead1b43a29f966690639fab10224ecf56
sebcourtois <neitsabes974@gmail.com>
2015-09-16 09:40:36 +0200

    updated and fixed "damas.py". updated "zombillenium.py" to use DualAuth class for project authentication.

--- python/zomblib/config/zombillenium.py
+++ python/zomblib/config/zombillenium.py
@@ -22,8 +22,8 @@ class project:
     child_sections = libraries
 
     shotgun_class = "zomblib.shotgunengine.ShotgunEngine"
-    authenticator_class = ".authtypes.ShotgunAuth"
-    #no_damas = True
+    authenticator_class = ".authtypes.DualAuth"
+
 
 class asset_lib:
 
