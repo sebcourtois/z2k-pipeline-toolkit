4d7e47deaa679548c7034c7ff4e86e8846da4e92
sebcourtois <neitsabes974@gmail.com>
2015-09-24 17:19:47 +0200

    updated Maya Davos menu

--- maya_mods/zombie/python/davos_maya/tool/davossetup.py
+++ maya_mods/zombie/python/davos_maya/tool/davossetup.py
@@ -1,14 +1,17 @@
 
+import pymel.core as pm
 
 from pytaya.util.toolsetup import ToolSetup
-from davos_maya.tool import file_browser
+#from pytd.gui.dialogs import confirmDialog
+#from pytd.util.sysutils import toStr
 
-import pymel.core as pm
+from davos_maya.tool import file_browser
+from davos_maya.tool import publishing
 
 class DavosSetup(ToolSetup):
 
     classMenuName = "davosMenu"
-    classMenuLabel = "Davos Tools"
+    classMenuLabel = "Davos"
 
     def __init__(self):
         super(DavosSetup, self).__init__()
@@ -16,6 +19,7 @@ class DavosSetup(ToolSetup):
     def populateMenu(self):
 
         with self.menu:
-            pm.menuItem(label="File Browser", c=file_browser.launch)
+            pm.menuItem(label="Asset Browser", c=file_browser.launch)
+            pm.menuItem(label="Publish...", c=publishing.publishCurrentScene)
 
         ToolSetup.populateMenu(self)
--- maya_mods/zombie/python/davos_maya/tool/publishing.py
+++ maya_mods/zombie/python/davos_maya/tool/publishing.py
@@ -1,15 +1,20 @@
 
 import os
 
-import pymel.core as pm
+#from pytd.util.logutils import logMsg
+from pytaya.core import system as myasys
 
 from davos.core.damproject import DamProject
+
 
 def publishCurrentScene(*args):
 
     sProject = os.environ["DAVOS_INIT_PROJECT"]
     proj = DamProject(sProject)
 
-    sScnPath = pm.sceneName()
-    proj.publishEditedVersion(sScnPath)
+    sScenePath = myasys.saveScene(confirm=False)
+    if not sScenePath:
+        raise RuntimeError("Could not save your current scene !")
+
+    proj.publishEditedVersion(sScenePath)
 
