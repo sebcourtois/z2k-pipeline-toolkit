eda14dac7dab79aab5f420739c1e2460f31dbe5b
sebcourtois <neitsabes974@gmail.com>
2015-10-13 16:08:09 +0200

    configured shotgun version creation for "previz_scene" and "modeling_scene".

--- python/zomblib/config/zombillenium.py
+++ python/zomblib/config/zombillenium.py
@@ -65,13 +65,13 @@ class shot_lib(object):
                      "{name}_sound.wav -> animatic_sound":None,
                      "{name}_animatic.mov -> animatic_capture":None,
                     },
-                 "{step=01_previz} -> previz_dir":
+                 "{step:01_previz} -> previz_dir":
                     {
                      "export -> previz_export_dir":{},
                      "{name}_previz.ma -> previz_scene":None,
                      "{name}_previz.mov -> previz_capture":None,
                     },
-                 "{step=02_layout} -> layout_dir":
+                 "{step:02_layout} -> layout_dir":
                     {
                      "{name}_layout.ma -> layout_scene":None,
                      "{name}_layout.mov -> layout_capture":None,
@@ -124,6 +124,13 @@ class asset_lib(object):
 
     entity_dir = "{assetType}/{name}"
 
+    resources_settings = {
+    "previz_scene":{"create_sg_version":True,
+                    "sg_step":"Model Previz", },
+    "modeling_scene":{"create_sg_version":True,
+                      "sg_step":"Model HD", },
+    }
+
 class camera(object):
 
     prefix = "cam"
@@ -174,13 +181,7 @@ class charbase(object):
         },
     }
 
-    resources_settings = {
-    "previz_scene":{
-                    "create_sg_version":True,
-                    "sg_step":"Model Previz",
-                    #"upload_to_sg":"preview_image"
-                    },
-    }
+    resources_settings = asset_lib.resources_settings
 
 class character3d(charbase):
 
@@ -227,6 +228,8 @@ class prop3d(object):
         },
     }
 
+    resources_settings = asset_lib.resources_settings
+
 class vehicle3d(prop3d):
 
     entity_class = "davos.core.damtypes.DamAsset"
@@ -262,6 +265,13 @@ class set3d(object):
         "{name}_previz.ma -> previz_scene":None,
         "{name}_master.ma -> master_scene":None,
         },
+    }
+
+    resources_settings = {
+    "previz_scene":{"create_sg_version":True,
+                    "sg_step":"Model Previz", },
+    "master_scene":{"create_sg_version":True,
+                      "sg_step":"Model HD", },
     }
 
 class environment3d(set3d):
