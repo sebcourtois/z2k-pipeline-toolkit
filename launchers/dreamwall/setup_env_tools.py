6cf3ddf4191d3f722703e0f9195297a832e29218
sebcourtois <neitsabes974@gmail.com>
2015-10-19 16:13:20 +0200

    defined "ZOMB_MISC_LOC" into dreamwall/setup_env_tools.py

--- launchers/dreamwall/setup_env_tools.py
+++ launchers/dreamwall/setup_env_tools.py
@@ -1,7 +1,6 @@
 import os
 osp = os.path
 import sys
-import subprocess
 
 sys.path.append(osp.join(osp.dirname(__file__), ".."))
 from ztk_setup import Z2kToolkit
@@ -11,6 +10,7 @@ ENVS = {
         "ZOMB_ASSET_LOC":"\\\\lv7000_fileserver\\Zombidamas",
         "ZOMB_SHOT_LOC":"\\\\lv7000_fileserver\\Zombidamas",
         "ZOMB_OUTPUT_LOC":"\\\\lv7000_fileserver\\Zombidamas",
+        "ZOMB_MISC_LOC":"\\\\lv7000_fileserver\\Zombidamas",
         "ZOMB_PRIVATE_LOC":"\\\\lv7000_fileserver\\Zombidamas",
 
         "ZOMB_TOOL_PATH":"\\\\\lv7000_fileserver\\Zombidamas\\zomb\\tool",
