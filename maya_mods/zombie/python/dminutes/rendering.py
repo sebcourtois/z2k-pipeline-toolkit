5955231f5d2fa056c01d64286b0871da976f9a08
albermond <al.bermond@mykolab.com>
2015-11-25 16:05:09 +0100

    getRenderOutput: file version is now found a different way

--- maya_mods/zombie/python/dminutes/rendering.py
+++ maya_mods/zombie/python/dminutes/rendering.py
@@ -182,7 +182,8 @@ def getRenderOutput(gui = True):
     if mc.ls("|asset"):        
         if  mainFilePathElem[-4] == "asset":
             departement =  mainFilePathElem[-1].split("_")[-1].split(".")[0].split("-")[0]
-            version =  mainFilePathElem[-1].split("_")[-1].split(".")[0].split("-")[1]
+            #version =  mainFilePathElem[-1].split("_")[-1].split(".")[0].split("-")[1]
+            version =  mainFilePathElem[-1].split("-")[1][:4]
             if departement not in ["modeling","anim","previz","render","master"]:
                 myMessage = "#### {:>7}: '{}' is not a valid scene name, file type '{}' is not valid and cannot be used to define the render path".format("Error",mainFilePathElem[-1], departement)
                 if gui == True:
