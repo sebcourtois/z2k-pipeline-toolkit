428c3965190dff02f71c1886232b6bfa606a9590
albermond <al.bermond@mykolab.com>
2015-12-03 16:44:36 +0100

    cleanAsset.pynow handle aovcleaning properly

--- maya_mods/zombie/scripts/zombie/misc/cleanAsset.py
+++ maya_mods/zombie/scripts/zombie/misc/cleanAsset.py
@@ -1,17 +1,26 @@
-from dminutes import assetconformation
-reload (assetconformation)
+import maya.cmds as mc
 
 from dminutes import modeling
 reload(modeling)
 
+from dminutes import assetconformation
+reload(assetconformation)
 
-answer =  mc.confirmDialog( title='Confirm', message="You are about to delete all the node that is not related to the asset structure, delete geometry history and clean the namespaces", button=['Proceed','Cancel'], defaultButton='Proceed', cancelButton='Cancel', dismissString='Cancel' )
-	if answer == "Cancel": 
-        return
+from dminutes import rendering
+reload (rendering)
+
+from dminutes import miscUtils
+reload (miscUtils)
 
 
-assetconformation.softClean()
 
-modeling.geoGroupDeleteHistory()
-modeling.makeAllMeshesUnique(inParent="|asset|grp_geo")
-modeling.meshShapeNameConform(inParent = "|asset|grp_geo")
+
+
+answer =  mc.confirmDialog( title='Confirm', message="You are about to delete all the nodes that are not related to the asset structure, delete geometry history and clean the namespaces", button=['Proceed','Cancel'], defaultButton='Proceed', cancelButton='Cancel', dismissString='Cancel' )
+if answer != "Cancel": 
+	rendering.cleanAovs()
+	assetconformation.softClean()
+	modeling.geoGroupDeleteHistory()
+	modeling.makeAllMeshesUnique(inParent="|asset|grp_geo")
+	modeling.meshShapeNameConform(inParent = "|asset|grp_geo")
+	miscUtils.deleteUnknownNodes()
