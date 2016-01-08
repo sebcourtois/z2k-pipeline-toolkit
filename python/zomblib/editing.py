92c97f6d348c2a9eb41f32264ebc110f24df0bba
sebcourtois <neitsabes974@gmail.com>
2016-01-08 17:52:18 +0100

    implemented tool to compare edl shots vs shotgun shots.

---
+++ python/zomblib/editing.py
@@ -0,0 +1,187 @@
+
+import os
+import re
+from datetime import datetime
+
+osp = os.path
+
+FPS = 24.0
+TC_SEPARATOR = ":"
+
+def convertTcToSecondsTc(in_Tc, in_fps=FPS, in_iHoursOffset=0, in_iMinutesOffset=0,
+                         in_iSecondsOffset=0, in_iFramesOffset=0):
+
+    hours, minutes, seconds, frames = [int(val) for val in in_Tc.split(TC_SEPARATOR)]
+
+    frames += in_iFramesOffset
+    if frames < 0:
+        frames += FPS
+        in_iSecondsOffset -= 1
+    elif frames >= FPS:
+        frames -= FPS
+        in_iSecondsOffset += 1
+
+    seconds += in_iSecondsOffset
+    if seconds < 0:
+        seconds += 60
+        in_iMinutesOffset -= 1
+    elif seconds >= 60:
+        seconds -= 60
+        in_iMinutesOffset += 1
+
+    minutes += in_iMinutesOffset
+    if minutes < 0:
+        minutes += 60
+        in_iHoursOffset -= 1
+    elif minutes >= 60:
+        minutes -= 60
+        in_iHoursOffset += 1
+
+    hours += in_iHoursOffset
+
+    #print "{0} {1} {2} {3}".format(hours, minutes, seconds, frames)
+
+    return "{1}{0}{2}{0}{3}".format(TC_SEPARATOR, hours, minutes, seconds + frames / FPS)
+
+def convertTcToSeconds(in_Tc, in_fps=FPS, in_iHoursOffset=0, in_iMinutesOffset=0,
+                       in_iSecondsOffset=0, in_iFramesOffset=0):
+
+    hours, minutes, seconds, frames = [int(val) for val in in_Tc.split(TC_SEPARATOR)]
+
+    frames += in_iFramesOffset
+    if frames < 0:
+        frames += FPS
+        in_iSecondsOffset -= 1
+    elif frames >= FPS:
+        frames -= FPS
+        in_iSecondsOffset += 1
+
+    seconds += in_iSecondsOffset
+    if seconds < 0:
+        seconds += 60
+        in_iMinutesOffset -= 1
+    elif seconds >= 60:
+        seconds -= 60
+        in_iMinutesOffset += 1
+
+    minutes += in_iMinutesOffset
+    if minutes < 0:
+        minutes += 60
+        in_iHoursOffset -= 1
+    elif minutes >= 60:
+        minutes -= 60
+        in_iHoursOffset += 1
+
+    hours += in_iHoursOffset
+
+    return hours * 3600 + minutes * 60 + seconds + frames / FPS
+
+def parseEdl(in_sEdlPath, in_sSeqFilter=None):
+
+    shots = []
+
+    with open(in_sEdlPath, 'r') as f:
+        lines = f.readlines()
+
+    tcRegEx = re.compile("(\d{3})\s+GEN\s+V\s+C\s+\d{2}:\d{2}:\d{2}:\d{2}\s+\d{2}:\d{2}:\d{2}:\d{2}\s+(\d{2}:\d{2}:\d{2}:\d{2})\s+(\d{2}:\d{2}:\d{2}:\d{2})")
+    shotRegEx = re.compile(".*FROM CLIP NAME:  (SQ\d{4}[a-zA-Z]?)\s\s(P\d{4}[a-zA-Z]?)")
+    seqRegEx = re.compile(".*FROM CLIP NAME:  (SQ\d{4}[a-zA-Z]?)")
+
+    numLines = len(lines)
+
+    for counter in range(numLines):
+        matchObj = tcRegEx.match(lines[counter])
+        if matchObj:
+            shotMatchObj = shotRegEx.match(lines[counter + 1])
+
+            if shotMatchObj:
+                shots.append({
+                            "index":matchObj.group(1).strip(),
+                            "sequence":shotMatchObj.group(1).strip(),
+                            "shot":shotMatchObj.group(2).strip(),
+                            "start":matchObj.group(2).strip(),
+                            "end":matchObj.group(3).strip()
+                            })
+            else:
+                seqMatchObj = seqRegEx.match(lines[counter + 1])
+                if seqMatchObj:
+                    shots.append({
+                                "index":matchObj.group(1).strip(),
+                                "sequence":seqMatchObj.group(1).strip(),
+                                "shot":'P0000A',
+                                "start":matchObj.group(2).strip(),
+                                "end":matchObj.group(3).strip()
+                                })
+                else:
+                    print ("Error reading shot info for line {0} ({1})"
+                            .format(counter, lines[counter + 1].replace('\n', '')))
+                    return None
+    return shots
+
+
+def h264ToProres(inSeqList=['sq0230', 'sq0150'], shotStep='01_previz'):
+    """
+    There are 4 profiles that exist within Prores: Proxy, LT, SQ and HQ (and then optionally 4444). In ffmpeg these profiles are assigned numbers (0 is Proxy and 3 is HQ)
+    """
+    ffmpegCommand = osp.normpath(osp.join(os.environ["Z2K_LAUNCH_SCRIPT"].split("launchers")[0], "movSplitter", "ffmpeg", "bin", "ffmpeg.exe"))
+    shotDir = osp.normpath(osp.join(os.environ["ZOMB_SHOT_LOC"], "zomb", "shot"))
+    montageDir = "//Zombiwalk/z2k/11_EXCHANGE_MONTAGE"
+    sSeqShotDict = {}
+
+    if shotStep == "01_previz" or shotStep == "02_layout":
+        profile = 0
+        shotExt = shotStep.split("_")[-1]
+    else:
+        raise ValueError("'{}' shotStep is not valid".format(shotStep))
+
+    for eachSeq in os.listdir(shotDir):
+        if re.match('^sq[0-9]{4}$', eachSeq) and (eachSeq in inSeqList or not inSeqList):
+            for eachShot in os.listdir(osp.normpath(osp.join(shotDir, eachSeq))):
+                if re.match('^sq[0-9]{4}_sh[0-9]{4}a$', eachShot):
+                    #sSeqShotDict[eachSeq].append(eachShot)
+                    sSeqShotDict.setdefault(eachSeq, []).append(eachShot)
+
+    for each in inSeqList:
+        if each not in sSeqShotDict.keys():
+            raise ValueError("'{}' sequence could not be found".format(each))
+
+    tempBatFile = osp.normpath(osp.join(os.environ["temp"], "conv2prores.bat"))
+    print "#### {:>7}: writing temp batch file: '{}'".format("Info", tempBatFile)
+
+    oDate = datetime.today()
+
+    with open(tempBatFile, "w") as conv2prores_obj:
+
+        for seqName, shotNameList in sSeqShotDict.items():
+
+            sDate = str(oDate.year) + "-" + str(oDate.month) + "-" + str(oDate.day)
+            outDir = osp.normpath(osp.join(montageDir, shotStep, seqName, sDate))
+
+            conv2prores_obj.write("\n")
+
+            if  not osp.isdir(outDir):
+                #print "#### {:>7}: Create directory: '{}'".format("Info",outDir )
+                os.makedirs(outDir)
+
+            for shotName in shotNameList:
+                inDir = osp.normpath(osp.join(shotDir, seqName, shotName, shotStep, "_version"))
+                if  not osp.isdir(inDir):
+                    raise ValueError("Directory could not be found: '{}'".format(inDir))
+                videoList = []
+                for each in os.listdir(inDir):
+                    if re.match('^sq[0-9]{4}_sh[0-9]{4}[a-z]{1}_[a-zA-Z0-9\-]{1,24}.mov$', each):
+                        videoList.append(each)
+                if len (videoList) < 2:
+                    print "#### {:>7}: no video found in directory: '{}'".format("Warning", inDir)
+                    continue
+                videoList.sort()
+                inFile = osp.normpath(osp.join(inDir, videoList[-1]))
+                outFile = osp.normpath(osp.join(outDir, videoList[-1]))
+                finalCommand = ("{0} -i {1} -c:v prores_ks -profile:v {2} {3}"
+                                .format(ffmpegCommand, inFile, profile, outFile))
+                conv2prores_obj.write(finalCommand + "\n")
+
+        conv2prores_obj.write("\n")
+        conv2prores_obj.write("pause\n")
+
+    #subprocess.call([tempBatFile])
---
+++ scripts/edl_vs_shotgun.py
@@ -0,0 +1,120 @@
+
+import os
+import sys
+from collections import OrderedDict
+import csv
+
+from zomblib.editing import parseEdl, convertTcToSeconds, FPS
+
+#from pytd.util.sysutils import inDevMode
+#from pytd.util.fsutils import copyFile
+
+from davos.core.damproject import DamProject
+#from davos.core.damtypes import DamShot
+
+def launch():
+
+    sEdlPath = ""
+    sCsvPath = ""
+    if len(sys.argv) == 2:
+        sEdlPath = sys.argv[1]
+        sPath, sExt = os.path.splitext(sEdlPath)
+        if sExt.lower() == ".edl":
+            sCsvPath = sPath + "_status.csv"
+
+    if not (sCsvPath and sEdlPath):
+        raise RuntimeError("Please, drag and drop an EDL file on me.")
+
+    proj = DamProject(os.environ.get("DAVOS_INIT_PROJECT"))
+    sg = proj._shotgundb.sg
+
+    statusNameDct = dict((d["code"], d["name"]) for d in  sg.find("Status", [], ["code", "name"]))
+
+    sSeqList = []
+    edlShotDct = OrderedDict()
+
+    for edlShot in parseEdl(sEdlPath):
+
+        sSeqCode = edlShot["sequence"].lower()
+        sShotName = "sh" + edlShot["shot"].lstrip("P").lower()
+        sShotCode = sSeqCode + "_" + sShotName
+
+        startTime = convertTcToSeconds(edlShot["start"], in_iHoursOffset=-1)
+        endTime = convertTcToSeconds(edlShot["end"], in_iHoursOffset=-1, in_iFramesOffset=-1)
+        numFrames = ((endTime - startTime) * FPS) + 1
+        edlShot["duration"] = numFrames
+
+        sSeqList.append(sSeqCode)
+        edlShotDct[sShotCode] = edlShot
+
+    if not sSeqList:
+        raise RuntimeError("No sequences found in '{}'.".format(sEdlPath))
+
+    print "Retrieving Shotgun data..."
+    filters = [["sg_sequence.Sequence.code", "in", sSeqList], ["code", "ends_with", "a"]]
+    fields = ["sg_cut_duration"]
+    sgShotDct = OrderedDict((d["code"], d) for d in proj.listAllSgShots(moreFilters=filters,
+                                                                        moreFields=fields))
+
+    filters = [["entity.Shot.code", "in", sgShotDct.keys()], ["content", "is", "previz 3D"]]
+    fields = ["entity.Shot.code", "sg_status_list"]#, "content"]
+    sgPrevizTaskDct = dict((d.pop("entity.Shot.code"), d) for d in sg.find("Task", filters, fields))
+
+    sAllShotList = sorted(set(edlShotDct.iterkeys()) | set(sgShotDct.iterkeys()))
+
+    with open(sCsvPath, "wb") as csvFile:
+
+        writer = csv.writer(csvFile, delimiter="|")
+
+        header = ["Shot Code", "Report", "EDL Duration", "SG Duration", "Previz Status"]
+        writer.writerow(header)
+
+        sPrevSeqCode = ""
+        numShots = len(sAllShotList)
+        c = 0
+        for sShotCode in sAllShotList:
+
+            c += 1
+            print "Processing {}/{} shot: '{}'...".format(c, numShots, sShotCode)
+
+            sSeqCode = sShotCode.split("_", 1)[0]
+            if sPrevSeqCode and sSeqCode != sPrevSeqCode:
+                writer.writerow([])
+
+            sPrevSeqCode = sSeqCode
+
+            bSgShot = sShotCode in sgShotDct
+            bEdlShot = sShotCode in edlShotDct
+
+            sReportMsg = ""
+            sEdlDuration = ""
+            sSgDuration = ""
+            sPrevizStatus = ""
+
+            if bSgShot:
+                sSgDuration = "{:.0f}".format(sgShotDct[sShotCode]["sg_cut_duration"])
+                sPrevizStatus = statusNameDct[sgPrevizTaskDct[sShotCode]["sg_status_list"]]
+                if not bEdlShot:
+                    sReportMsg = "omitted shot"
+
+            if bEdlShot:
+                sEdlDuration = "{:.0f}".format(edlShotDct[sShotCode]["duration"])
+                if not bSgShot:
+                    sReportMsg = "added shot"#"not in shotgun"
+
+            if bSgShot and bEdlShot:
+                if sSgDuration != "1" and sEdlDuration != sSgDuration:
+                    sReportMsg = "updated duration"
+
+            row = [sShotCode, sReportMsg, sEdlDuration, sSgDuration, sPrevizStatus]
+            writer.writerow(row)
+
+    os.system(sCsvPath)
+
+if __name__ == "__main__":
+    try:
+        launch()
+    except:
+        os.environ["PYTHONINSPECT"] = "1"
+        raise
+
