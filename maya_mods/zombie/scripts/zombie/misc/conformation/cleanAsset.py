5cd47cc96203d1f18a96152e666cc09f902d1b0a
albermond <al.bermond@mykolab.com>
2016-01-28 17:17:03 +0100

    a la carte 'clean asset'

--- maya_mods/zombie/scripts/zombie/misc/conformation/cleanAsset.py
+++ maya_mods/zombie/scripts/zombie/misc/conformation/cleanAsset.py
@@ -1,27 +1,5 @@
-import maya.cmds as mc
-
-from dminutes import modeling
-reload(modeling)
-
 from dminutes import assetconformation
 reload(assetconformation)
 
-from dminutes import rendering
-reload (rendering)
 
-from dminutes import miscUtils
-reload (miscUtils)
-
-
-
-
-
-answer =  mc.confirmDialog( title='Confirm', message="You are about to delete all the nodes that are not related to the asset structure,\n delete geometry history, all the colors sets and clean the namespaces", button=['Proceed','Cancel'], defaultButton='Proceed', cancelButton='Cancel', dismissString='Cancel' )
-if answer != "Cancel": 
-	rendering.deleteAovs()
-	assetconformation.softClean()
-	modeling.geoGroupDeleteHistory()
-	modeling.makeAllMeshesUnique(inParent="|asset|grp_geo")
-	modeling.meshShapeNameConform(inParent = "|asset|grp_geo")
-	miscUtils.deleteUnknownNodes()
-	miscUtils.deleteAllColorSet()
+assetconformation.cleanAsset(GUI=True)
