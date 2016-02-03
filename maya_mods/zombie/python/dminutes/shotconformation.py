a66165d28c69e422e935a7da2d0510a562b08193
albermond <al.bermond@mykolab.com>
2016-02-03 17:40:42 +0100

    removeRefEditByAttr improvement

--- maya_mods/zombie/python/dminutes/shotconformation.py
+++ maya_mods/zombie/python/dminutes/shotconformation.py
@@ -1,22 +1,38 @@
 import maya.cmds as mc
 
-def removeRefEditByAttr(refNodeL=[], attr= "smoothDrawType"):
+def removeRefEditByAttr(inRefNodeL=[], attr= "smoothDrawType", GUI = True):
+	resultB = True
+	logL = []
+	refNodeL = []
 
-	if not refNodeL:
-	    refNodeL= mc.ls(type = "reference")
+	if not inRefNodeL:
+	    inRefNodeL= mc.ls(references = True)
+
+	for each in inRefNodeL:
+		if mc.referenceQuery(each,filename=True):
+			refNodeL.append(each)
 
 	for eachRefNode in refNodeL:
 		myAttrRefEditL = []
 		allRefEditL = mc.referenceQuery( eachRefNode, editStrings=True)
+		refIsLoaded = mc.referenceQuery( eachRefNode,isLoaded= True )
 
 		for eachRefEdit in allRefEditL:
 		    if "."+attr in eachRefEdit:
 		        myAttrRefEditL.append(eachRefEdit.split(" ")[1])
 
+		if refIsLoaded and myAttrRefEditL : mc.file( unloadReference=eachRefNode )
 		for each in myAttrRefEditL:
-		    mc.referenceEdit( each, failedEdits = True, successfulEdits = True, editCommand = 'setAttr', removeEdits = True )
+			mc.referenceEdit( each, failedEdits = True, successfulEdits = True, editCommand = 'setAttr', removeEdits = True )
+		if refIsLoaded and myAttrRefEditL: mc.file( loadReference=eachRefNode )
 
-		print "#### {:>7}: 'removeRefEditByAttr': '{:>40}', {} '{}' reference edit deleted".format("Info",eachRefNode,len(myAttrRefEditL),attr )
+		logMessage ="#### {:>7}: 'removeRefEditByAttr': '{:>40}', {} '{}' reference edit deleted".format("Info",eachRefNode,len(myAttrRefEditL),attr )
+		logL.append(logMessage)
+
+	if GUI == True:
+		for each in logL:
+			print each
+	return resultB, logL
 	    
 	    
 
