ad3978ef00859fa05a26b135f071064152988c91
albermond <al.bermond@mykolab.com>
2016-02-19 19:39:40 +0100

    minor fix

--- maya_mods/zombie/python/dminutes/modeling.py
+++ maya_mods/zombie/python/dminutes/modeling.py
@@ -613,7 +613,7 @@ def geoGroupDeleteHistory(GUI=True, freezeVrtxPos = True):
     logL = []
     geoTransformList,instanceTransformL = miscUtils.getAllTransfomMeshes(inParent = "|asset|grp_geo")
 
-    #process in a different loop for instances to avoid precessing the several time
+    #now process vertex frezze position in a different loop for instances to avoid precessing the several time
     if freezeVrtxPos:
         for each in geoTransformList:
             cmds.polyMoveVertex (each, constructionHistory =True, random  = 0)
@@ -708,6 +708,7 @@ def freezeResetTransforms(inParent = "*", inVerbose = True, inConform = False, G
         if GUI == True: print logMessage
 
     return resultB, logL
+
 
 def compareHDToPreviz():
     """
