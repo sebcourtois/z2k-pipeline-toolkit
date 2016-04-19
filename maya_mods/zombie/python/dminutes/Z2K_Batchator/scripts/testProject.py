e0bf0398782fcbb3b623f798aab1b30be26603c8
albermond <al.bermond@mykolab.com>
2016-04-19 11:45:15 +0200

    now pointing toward v014

--- maya_mods/zombie/python/dminutes/Z2K_Batchator/scripts/testProject.py
+++ maya_mods/zombie/python/dminutes/Z2K_Batchator/scripts/testProject.py
@@ -1,9 +1,9 @@
 # TEST PROJECT ENV
 import os
 toReturnB = False
-import dminutes.Z2K_Batchator.Z2K_Release_Batch_CONFIG_v13 as Batch_CONFIG
+import dminutes.Z2K_Batchator.Z2K_Release_Batch_CONFIG_v14 as Batch_CONFIG
 reload(Batch_CONFIG)
-from dminutes.Z2K_Batchator.Z2K_Release_Batch_CONFIG_v13 import *
+from dminutes.Z2K_Batchator.Z2K_Release_Batch_CONFIG_v14 import *
 
 def printF(texta="",openMode = "a",printN=True,*args,**kwargs):
     debugFile=DEBUGFILE
@@ -20,13 +20,10 @@ try:
     printF(texta="curproj={0}".format(curproj), openMode="a", printN=True)
 except Exception,err:
     printF(texta="ERROR_curproj:\r\t{0}".format(err), openMode="a", printN=True)
-    
-
+   
 
 
 if curproj:
     toReturnB = True
 
 result= toReturnB
-
-
