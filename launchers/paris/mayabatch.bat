5259c17d332eaf4c4c639d2dc33c243654945372
sebcourtois <neitsabes974@gmail.com>
2016-06-21 11:08:52 +0200

    passes command line arguments to mayabatch.exe

--- launchers/paris/mayabatch.bat
+++ launchers/paris/mayabatch.bat
@@ -1,1 +1,1 @@
-"C:\Python27\python.exe" "%~dp0\setup_env_tools.py" launch "C:\Program Files\Autodesk\Maya2016\bin\mayabatch.exe" -prompt
+"C:\Python27\python.exe" "%~dp0\setup_env_tools.py" launch "C:\Program Files\Autodesk\Maya2016\bin\mayabatch.exe" %*
