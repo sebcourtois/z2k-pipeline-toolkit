cd3405416f70ecddc9d6f7208520b039828c6b68
sebcourtois <neitsabes974@gmail.com>
2016-07-20 18:35:48 +0200

    updated paths in cache export launchers

--- launchers/paris/cache_exporters/export_from_FL.bat
+++ launchers/paris/cache_exporters/export_from_FL.bat
@@ -1,1 +1,1 @@
-"C:\Python27\python.exe" "%~dp0\setup_env_tools.py" launch "C:\Python27\python.exe" "%~dp0\..\..\scripts\abc_exporter.py" finalLayout_scene %*
+"C:\Python27\python.exe" "%~dp0\..\setup_env_tools.py" launch "C:\Python27\python.exe" "%~dp0\..\..\..\scripts\abc_exporter.py" finalLayout_scene %*
