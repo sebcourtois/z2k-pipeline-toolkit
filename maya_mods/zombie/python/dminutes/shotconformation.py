abd64efbd67ccfbba80a0cc277b209ac68635152
albermond <al.bermond@mykolab.com>
2016-08-03 20:13:38 +0200

    added finalLayoutToLighting() removeRefEditByAttr() now also manage to remove failing reference edits

--- maya_mods/zombie/python/dminutes/shotconformation.py
+++ maya_mods/zombie/python/dminutes/shotconformation.py
@@ -1,13 +1,13 @@
 import maya.cmds as mc
+import pymel.core as pm
 
 from dminutes import miscUtils
 reload (miscUtils)
 
 
 
-def removeRefEditByAttr(inRefNodeL=[], attr="smoothDrawType", cmd="setAttr", GUI=True):
-    resultB = True
-    logL = []
+def removeRefEditByAttr(inRefNodeL=[], attr="smoothDrawType", cmd="setAttr",failedRefEdit =False, GUI=True):
+    log = miscUtils.LogBuilder(gui=GUI, funcName ="removeRefEditByAttr")
     refNodeL = []
 
     sAttrList = attr if not isinstance(attr, basestring) else [attr]
@@ -25,11 +25,8 @@ def removeRefEditByAttr(inRefNodeL=[], attr="smoothDrawType", cmd="setAttr", GUI
             refNodeL.append(each)
 
     for eachRefNode in refNodeL:
-
         for sCmd in sCmdList:
-
-            sNodeAttrList = mc.referenceQuery(eachRefNode, editCommand=sCmd,
-                                              editNodes=True, editAttrs=True)
+            sNodeAttrList = mc.referenceQuery(eachRefNode, editCommand=sCmd, editNodes=True, editAttrs=True)
 
             sNodeAttrDct = dict()
             for sNodeAttr in sNodeAttrList:
@@ -47,20 +44,45 @@ def removeRefEditByAttr(inRefNodeL=[], attr="smoothDrawType", cmd="setAttr", GUI
             try:
                 for sAttr, sNodeAttrSet in sNodeAttrDct.iteritems():
                     for sNodeAttr in sNodeAttrSet:
-                        mc.referenceEdit(sNodeAttr, editCommand=sCmd, removeEdits=True,
-                                         failedEdits=True, successfulEdits=True)
-                    sMsg = ("#### {:>7}: 'removeRefEditByAttr': '{:>40}', {} '{} .{}' reference edit deleted"
-                                  .format("Info", eachRefNode, len(sNodeAttrSet) , sCmd, sAttr))
-                    print sMsg
-                    logL.append(sMsg)
+                        mc.referenceEdit(sNodeAttr, editCommand=sCmd, removeEdits=True,failedEdits=True, successfulEdits=True)
+                        
+                        sMsg = "'{}', '{} .{}' {} reference edit deleted".format(eachRefNode , sCmd, sAttr, len(sNodeAttrSet))
+                    log.printL("i", sMsg)
             finally:
                 if refIsLoaded:
                     mc.file(loadReference=eachRefNode)
 
-    if GUI == True:
-        for each in logL:
-            print each
+        if failedRefEdit:
+            mc.referenceEdit(eachRefNode, removeEdits=True,failedEdits=True,successfulEdits = False)
+            sMsg = "'{}'  all failing reference edit  removed".format(eachRefNode)
+            log.printL("i", sMsg)
 
-    return resultB, logL
+    for each in log.logL:
+        print each
 
+    return log.resultB, log.logL
+
+
+def finalLayoutToLighting(gui=True):
+    log = miscUtils.LogBuilder(gui=gui, funcName ="finalLayoutToLighting")
+    deletedNodeL=[]
+
+    mc.ls("mat_arelequin_*")+mc.ls("aiAOV_arlequin*")
+    toDeleteNodeL=mc.ls("mat_arelequin_*")+mc.ls("aiAOV_arlequin*")+mc.ls("lay_finalLayout_*")
+    for each in toDeleteNodeL: 
+        try: 
+            mc.delete(each)
+            deletedNodeL.append(each)
+        except Exception as err:
+            print err
+            pass
+
+    removeRefEditByAttr(inRefNodeL=[], attr="aovName", cmd="setAttr", failedRefEdit =True, GUI=True)
+    txt = "{} node(s) deleted: '{}': ".format(len(deletedNodeL),deletedNodeL)
+    log.printL("i", txt)
+
+    pm.mel.eval("setSoundDisplay audio 0")
+    log.printL("i", "sound turned off")
+
+    return dict(resultB=log.resultB, logL=log.logL)
 
