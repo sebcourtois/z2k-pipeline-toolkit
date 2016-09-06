05e9b621f2cca0fe242f59d5aaf0fe34f2e38c1a
jipee <jp_oim@hotmail.com>
2016-09-06 18:41:30 +0400

    CFX menu: pour de vrai raté sur le comit d'avant

---
+++ maya_mods/zombie/scripts/zombie/CFX/CFX_CLEAN_MeshCachePath.py
@@ -0,0 +1,6 @@
+﻿#! /usr/bin/python
+
+# Z2K clean CFX meshCachePath
+import dminutes.jipeLib_Z2K as jpZ
+reload(jpZ)
+jpZ.clean_CFX_cachePath()
---
+++ maya_mods/zombie/scripts/zombie/CFX/Z2K_ZmergencyTools/Select_HiddenKeyedCTR.py
@@ -0,0 +1,4 @@
+import dminutes.jipeLib_Z2K as jpZ
+reload(jpZ)
+
+jpZ.selectSpineFK0()
