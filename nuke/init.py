f34ed360d019cf568d8692ff9a64f1f1c66c166e
albermond <al.bermond@mykolab.com>
2016-09-29 18:27:57 +0200

    nouveau reglages

--- nuke/init.py
+++ nuke/init.py
@@ -1,15 +1,37 @@
-#
-#
-#  Copyright (c) 2014, 2015, 2016 Psyop Media Company, LLC
-#  See license.txt
-#
-#
-
 import os
-import cryptomatte_utilities
-cryptomatte_utilities.setup_cryptomatte()
+#import cryptomatte_utilities
+import sys
 
-userName = os.environ["USERNAME"]
-userPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/gizmos'
-if os.path.isdir( userPath ):
-    nuke.pluginAddPath( userPath )
+import nuke
+# import nkFinalLayout as nkFinalLayout
+# reload(nkFinalLayout)
+
+import nkU as nkU
+reload(nkU)
+
+
+# zombRootPath = os.environ["ZOMB_ROOT_PATH"]
+# user_name = os.environ["USER_NAME"] #davos user name
+
+# os.environ["ZOMB_ASSET_PATH"] = zombRootPath+"/zomb/asset"
+# os.environ["ZOMB_SHOT_PATH"] = zombRootPath+"/zomb/shot"
+# os.environ["ZOMB_OUTPUT_PATH"] = zombRootPath+"/zomb/output"
+# os.environ["ZOMB_MISC_PATH"] = zombRootPath+"/zomb/misc"
+# os.environ["ZOMB_TOOL_PATH"] = zombRootPath+"/zomb/tool"
+
+# os.environ["ZOMB_SHOT_LOC"] = zombRootPath+"/private/"+user_name+"/zomb/shot"
+
+nuke.addOnScriptLoad(nkU.initNukeShot)
+nuke.addBeforeRender(nkU.createWriteDir)
+
+
+#sys.path.append(os.path.join(zombRootPath+"/zomb/tool",r"z2k-pipeline-toolkit\python\davos-dev"))
+#sys.path.append(os.path.join(zombRootPath+"/zomb/tool",r"z2k-pipeline-toolkit\python\pypeline-tool-devkit"))
+
+#cryptomatte_utilities.setup_cryptomatte()
+
+# userName = os.environ["USERNAME"] #windows user name
+# userPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/gizmos'
+# if os.path.isdir( userPath ):
+#     nuke.pluginAddPath( userPath )
+
--- nuke/menu.py
+++ nuke/menu.py
@@ -5,8 +5,12 @@
 #
 #
 
-import cryptomatte_utilities
-cryptomatte_utilities.setup_cryptomatte_ui()
+#from davos.core.damproject import DamProject
+#import cryptomatte_utilities
+#cryptomatte_utilities.setup_cryptomatte_ui()
+
+import nkU as nkU
+reload(nkU)
 
 ## DmnToon
 nuke.menu('Nodes').addCommand('Z2K/LdvDmToon', lambda: nuke.createNode('LdvDmToon'))
@@ -15,3 +19,16 @@ nuke.menu('Nodes').addCommand('Z2K/MaskAovs', lambda: nuke.createNode('MaskAovs'
 
 ## Other Gizmos
 nuke.menu('Nodes').addCommand('Z2K/P_Matte', lambda: nuke.createNode('P_Matte'))
+
+
+## Scripts
+
+nuke.menu( 'Nuke' ).addCommand( 'Zombi/init nuke shot', lambda: nkU.initNukeShot() )
+nuke.menu( 'Nuke' ).addCommand( 'Zombi/conform file nodes', lambda: nkU.conformFileNode(readNodeL=nuke.allNodes('Read')+nuke.allNodes('Write'), gui=True, conformPathB = True) )
+nuke.menu( 'Nuke' ).addCommand( 'Zombi/publish/publish compositing', lambda: nkU.publishCompo(dryRun=False, gui = True) )
+nuke.menu( 'Nuke' ).addCommand( 'Zombi/publish/publish all inputs', lambda: nkU.publishNode(readNodeL=nuke.allNodes('Read'),guiPopUp = True) )
+nuke.menu( 'Nuke' ).addCommand( 'Zombi/publish/publish selected inputs', lambda: nkU.publishNode(readNodeL=nuke.selectedNodes('Read'),guiPopUp = True) )
+#myMenu = myToolbar.addMenu( 'zomb Tools' )
+#myToolbar.addCommand( 'init nuke shot', lambda: nkU.initNukeShot() )
+
+
