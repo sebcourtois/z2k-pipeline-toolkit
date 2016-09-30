52dc54bb173729f6403336f2531974b0a29df0b2
albermond <al.bermond@mykolab.com>
2016-09-30 18:05:27 +0200

    filalistion instal nuke

--- nuke/init.py
+++ nuke/init.py
@@ -1,25 +1,32 @@
 import os
-#import cryptomatte_utilities
 import sys
 
+import cryptomatte_utilities
+cryptomatte_utilities.setup_cryptomatte()
+
+userName = os.environ["USERNAME"] #windows user name
+
+
+gizmosPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/gizmos'
+if os.path.isdir( gizmosPath ):
+    nuke.pluginAddPath( gizmosPath )
+
+scriptsPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/scripts'
+print scriptsPath
+if os.path.isdir( scriptsPath ):
+    nuke.pluginAddPath( scriptsPath )
+
+
+
+
+
 import nuke
-# import nkFinalLayout as nkFinalLayout
-# reload(nkFinalLayout)
+import nkFinalLayout as nkFinalLayout
+reload(nkFinalLayout)
 
 import nkU as nkU
 reload(nkU)
 
-
-# zombRootPath = os.environ["ZOMB_ROOT_PATH"]
-# user_name = os.environ["USER_NAME"] #davos user name
-
-# os.environ["ZOMB_ASSET_PATH"] = zombRootPath+"/zomb/asset"
-# os.environ["ZOMB_SHOT_PATH"] = zombRootPath+"/zomb/shot"
-# os.environ["ZOMB_OUTPUT_PATH"] = zombRootPath+"/zomb/output"
-# os.environ["ZOMB_MISC_PATH"] = zombRootPath+"/zomb/misc"
-# os.environ["ZOMB_TOOL_PATH"] = zombRootPath+"/zomb/tool"
-
-# os.environ["ZOMB_SHOT_LOC"] = zombRootPath+"/private/"+user_name+"/zomb/shot"
 
 nuke.addOnScriptLoad(nkU.initNukeShot)
 nuke.addBeforeRender(nkU.createWriteDir)
