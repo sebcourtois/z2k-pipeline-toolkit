45952a39592b7f0b4cebfa5101ad52aeb70a2951
Dominique Coco <domsfx@gmail.com>
2016-09-30 15:06:59 +0200

    Activate Cryptomatte and Other Nuke gizmos

--- nuke/init.py
+++ nuke/init.py
@@ -1,13 +1,27 @@
 import os
-#import cryptomatte_utilities
 import sys
 
-import nuke
+import cryptomatte_utilities
+cryptomatte_utilities.setup_cryptomatte()
+
+userName = os.environ["USERNAME"] #windows user name
+gizmosPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/gizmos'
+if os.path.isdir( gizmosPath ):
+    nuke.pluginAddPath( gizmosPath )
+	
+## Bypassed 30092016
+# scriptsPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/scripts'
+# if os.path.isdir( scriptsPath ):
+    # nuke.pluginAddPath( scriptsPath )
+	 
+## Bypassed 30092016
+# import nuke
 # import nkFinalLayout as nkFinalLayout
 # reload(nkFinalLayout)
 
-import nkU as nkU
-reload(nkU)
+## Bypassed 30092016
+# import nkU as nkU
+# reload(nkU)
 
 
 # zombRootPath = os.environ["ZOMB_ROOT_PATH"]
@@ -21,17 +35,15 @@ reload(nkU)
 
 # os.environ["ZOMB_SHOT_LOC"] = zombRootPath+"/private/"+user_name+"/zomb/shot"
 
-nuke.addOnScriptLoad(nkU.initNukeShot)
-nuke.addBeforeRender(nkU.createWriteDir)
-
+## Bypassed 30092016
+# nuke.addOnScriptLoad(nkU.initNukeShot)
+# nuke.addBeforeRender(nkU.createWriteDir)
 
 #sys.path.append(os.path.join(zombRootPath+"/zomb/tool",r"z2k-pipeline-toolkit\python\davos-dev"))
 #sys.path.append(os.path.join(zombRootPath+"/zomb/tool",r"z2k-pipeline-toolkit\python\pypeline-tool-devkit"))
 
-#cryptomatte_utilities.setup_cryptomatte()
 
-# userName = os.environ["USERNAME"] #windows user name
-# userPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/gizmos'
-# if os.path.isdir( userPath ):
-#     nuke.pluginAddPath( userPath )
+
+
+
 
--- nuke/menu.py
+++ nuke/menu.py
@@ -4,30 +4,31 @@
 #  See license.txt
 #
 #
+import cryptomatte_utilities
+cryptomatte_utilities.setup_cryptomatte_ui()
 
 #from davos.core.damproject import DamProject
-#import cryptomatte_utilities
-#cryptomatte_utilities.setup_cryptomatte_ui()
 
-import nkU as nkU
-reload(nkU)
+
+## Bypassed 30092016
+# import nkU as nkU
+# reload(nkU)
 
 ## DmnToon
 nuke.menu('Nodes').addCommand('Z2K/LdvDmToon', lambda: nuke.createNode('LdvDmToon'))
 nuke.menu('Nodes').addCommand('Z2K/LyrCheck', lambda: nuke.createNode('LyrCheck'))
 nuke.menu('Nodes').addCommand('Z2K/MaskAovs', lambda: nuke.createNode('MaskAovs'))
 
-## Other Gizmos
+## P Matte Gizmos
 nuke.menu('Nodes').addCommand('Z2K/P_Matte', lambda: nuke.createNode('P_Matte'))
 
+## Scripts -- Bypassed 30092016
 
-## Scripts
-
-nuke.menu( 'Nuke' ).addCommand( 'Zombi/init nuke shot', lambda: nkU.initNukeShot() )
-nuke.menu( 'Nuke' ).addCommand( 'Zombi/conform file nodes', lambda: nkU.conformFileNode(readNodeL=nuke.allNodes('Read')+nuke.allNodes('Write'), gui=True, conformPathB = True) )
-nuke.menu( 'Nuke' ).addCommand( 'Zombi/publish/publish compositing', lambda: nkU.publishCompo(dryRun=False, gui = True) )
-nuke.menu( 'Nuke' ).addCommand( 'Zombi/publish/publish all inputs', lambda: nkU.publishNode(readNodeL=nuke.allNodes('Read'),guiPopUp = True) )
-nuke.menu( 'Nuke' ).addCommand( 'Zombi/publish/publish selected inputs', lambda: nkU.publishNode(readNodeL=nuke.selectedNodes('Read'),guiPopUp = True) )
+# nuke.menu( 'Nuke' ).addCommand( 'Zombi/init nuke shot', lambda: nkU.initNukeShot() )
+# nuke.menu( 'Nuke' ).addCommand( 'Zombi/conform file nodes', lambda: nkU.conformFileNode(readNodeL=nuke.allNodes('Read')+nuke.allNodes('Write'), gui=True, conformPathB = True) )
+# nuke.menu( 'Nuke' ).addCommand( 'Zombi/publish/publish compositing', lambda: nkU.publishCompo(dryRun=False, gui = True) )
+# nuke.menu( 'Nuke' ).addCommand( 'Zombi/publish/publish all inputs', lambda: nkU.publishNode(readNodeL=nuke.allNodes('Read'),guiPopUp = True) )
+# nuke.menu( 'Nuke' ).addCommand( 'Zombi/publish/publish selected inputs', lambda: nkU.publishNode(readNodeL=nuke.selectedNodes('Read'),guiPopUp = True) )
 #myMenu = myToolbar.addMenu( 'zomb Tools' )
 #myToolbar.addCommand( 'init nuke shot', lambda: nkU.initNukeShot() )
 
