672c6e6bf3d71b1a628f0b74f8c90e6812b726b0
albermond <al.bermond@mykolab.com>
2016-10-12 15:33:14 +0200

    added devspace mode

--- nuke/init.py
+++ nuke/init.py
@@ -1,26 +1,37 @@
 import os
 import sys
 
+import nuke
+
 import cryptomatte_utilities
 cryptomatte_utilities.setup_cryptomatte()
 
+
+
 userName = os.environ["USERNAME"] #windows user name
 
+if "DEVSPACE" in os.environ["NUKE_PATH"]:
+	print  "devmode", os.environ["NUKE_PATH"]
+	gizmosPath = 'C:/Users/' + userName + '/DEVSPACE/git/z2k-pipeline-toolkit/nuke/gizmos'
+	if os.path.isdir( gizmosPath ):
+	    nuke.pluginAddPath( gizmosPath )
 
-gizmosPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/gizmos'
-if os.path.isdir( gizmosPath ):
-    nuke.pluginAddPath( gizmosPath )
+	scriptsPath = 'C:/Users/' + userName + '/DEVSPACE/git/z2k-pipeline-toolkit/nuke/scripts'
+	print scriptsPath
+	if os.path.isdir( scriptsPath ):
+	    nuke.pluginAddPath( scriptsPath )
 
-scriptsPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/scripts'
-print scriptsPath
-if os.path.isdir( scriptsPath ):
-    nuke.pluginAddPath( scriptsPath )
+else:
+	gizmosPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/gizmos'
+	if os.path.isdir( gizmosPath ):
+	    nuke.pluginAddPath( gizmosPath )
+
+	scriptsPath = 'C:/Users/' + userName + '/zombillenium/z2k-pipeline-toolkit/nuke/scripts'
+	print scriptsPath
+	if os.path.isdir( scriptsPath ):
+	    nuke.pluginAddPath( scriptsPath )
 
 
-
-
-
-import nuke
 import nkFinalLayout as nkFinalLayout
 reload(nkFinalLayout)
 
