ec94e629cf7cc873e2a0bd0f6864a919300aa196
sebcourtois <neitsabes974@gmail.com>
2016-11-22 15:59:56 +0100

    fixed withErrorDialog(): toStr(e.args[-1])

--- maya_mods/zombie/python/dminutes/maya_scene_operations.py
+++ maya_mods/zombie/python/dminutes/maya_scene_operations.py
@@ -64,7 +64,7 @@ def withErrorDialog(func):
             raise
         except Exception as e:
             pc.confirmDialog(title='SORRY !',
-                             message=e.message,
+                             message=toStr(e.args[-1]),
                              button=["OK"],
                              defaultButton="OK",
                              cancelButton="OK",
