3b66c7b42ccc70baa0fe2181193de2b897a49322
sebcourtois <neitsabes974@gmail.com>
2016-12-05 17:09:43 +0100

    updated shotgun api from 3.0.21.dev to v3.0.32 now retries to upload to shotgun on URLError (5 attempts spaced by 3 secs).

--- third-party/shotgun_api3/sg_26.py
+++ third-party/shotgun_api3/sg_26.py
@@ -2,7 +2,7 @@ import sys
 import os
 import logging
 
-from .lib.httplib2 import Http, ProxyInfo, socks
+from .lib.httplib2 import Http, ProxyInfo, socks, SSLHandshakeError
 from .lib.sgtimezone import SgTimezone
 from .lib.xmlrpclib import Error, ProtocolError, ResponseError
 
@@ -26,10 +26,23 @@ except ImportError:
         sys.path.pop()
 
 
-import mimetypes    # used for attachment upload
-try:
-    mimetypes.add_type('video/webm','.webm') # try adding to test for unicode error
-except UnicodeDecodeError:
-    # Ticket #25579 python bug on windows with unicode
-    # Use patched version of mimetypes
+def _is_mimetypes_broken():
+    """
+    Checks if this version of Python ships with a broken version of mimetypes
+
+    :returns: True if the version of mimetypes is broken, False otherwise.
+    """
+    # mimetypes is broken on Windows only and for Python 2.7.0 to 2.7.9 inclusively.
+    # We're bundling the version from 2.7.10.
+    # See bugs :
+    # http://bugs.python.org/issue9291  <- Fixed in 2.7.7
+    # http://bugs.python.org/issue21652 <- Fixed in 2.7.8
+    # http://bugs.python.org/issue22028 <- Fixed in 2.7.10
+    return (sys.platform == "win32" and
+            sys.version_info[0] == 2 and sys.version_info[1] == 7 and
+            sys.version_info[2] >= 0 and sys.version_info[2] <= 9)
+
+if _is_mimetypes_broken():
     from .lib import mimetypes as mimetypes
+else:
+    import mimetypes
